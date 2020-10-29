49d21b24e0cbc852eefe95f9e4de14defe73d6db
Aldo Pacchiano <aldopacchiano@gmail.com>
2020-10-29 12:36:05 -0700

    asfdllkm

--- models.py
+++ models.py
@@ -23,7 +23,7 @@ from sklearn.linear_model import LogisticRegression
 #%matplotlib inline
 
 
-class BinaryLogisticRegression:
+class GLM:
     def __init__(self, random_init = False, fit_intercept=True, dim = None):
         self.fit_intercept = fit_intercept
         self.theta = None
@@ -113,6 +113,130 @@ class BinaryLogisticRegression:
         ### adjust this using the inverse_data_covariance
         ### for each datapoint return the max 
         return self.__sigmoid(np.dot(batch_X, self.theta) - self.__inverse_covariance_norm(batch_X, inverse_data_covariance))
+
+
+    def get_predictions(self, batch_X, threshold, inverse_data_covariance = []):
+      prob_predictions = self.predict_prob(batch_X, inverse_data_covariance )
+      thresholded_predictions= prob_predictions > threshold
+      return thresholded_predictions
+
+    def get_accuracy( self, batch_X, batch_y, threshold, inverse_data_covariance = []):
+      thresholded_predictions = self.get_predictions(batch_X, threshold, inverse_data_covariance)
+      return (thresholded_predictions == batch_y).mean()
+
+
+    def plot(self, x_min, x_max, num_points = 100):
+      x_space = np.linspace(x_min, x_max, num_points)
+      y_values = []
+      if self.theta.shape[0] == 2 and not self.fit_intercept:
+        for x in x_space:
+          y_values.append( -self.theta[0]/self.theta[1]*x )
+      elif self.theta.shape[0]==3 and self.fit_intercept:
+          for x in x_space:
+            y_values.append((-self.theta[0]*x - self.theta[2] )/self.theta[1] )
+      else:
+        print("Plotting not supported")
+        return 0
+      y_values = np.array(y_values)
+      plt.plot(x_space, y_values, color="black", label = "classifier")
+
+
+
+
+
+
+
+
+class BinaryLogisticRegression:
+    def __init__(self, random_init = False, fit_intercept=True, dim = None, alpha = 1):
+        self.fit_intercept = fit_intercept
+        self.theta = None
+        self.random_init = random_init
+        if dim != None:
+          if self.random_init:
+            self.theta = np.random.normal(0, 1, dim + fit_intercept)
+          else:
+            self.theta = np.zeros(dim + fit_intercept)
+        self.alpha = alpha        
+  
+    def __initialize_theta(self, batch_X):
+      if self.theta is None:    
+        if self.fit_intercept:
+          batch_X = self.__add_intercept(batch_X)
+        if self.random_init:
+          self.theta = np.random.normal(0, 1, batch_X.shape[1])
+        else:
+          self.theta = np.zeros(batch_X.shape[1])
+
+    def __add_intercept(self, batch_X):
+        intercept = np.ones((batch_X.shape[0], 1))
+        #print(np.concatenate((batch_X, intercept), axis=1) , " augmented batch")
+        #print(np.concatenate((batch_X, intercept), axis=1).shape , " augmented batch shape")
+        
+        return np.concatenate((batch_X, intercept), axis=1)
+    
+    def __sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
+    def __loss(self, h, y):
+        return (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
+    
+    def __inverse_covariance_norm(self, batch_X, inverse_covariance):
+      square_norm = np.dot(np.dot(batch_X, inverse_covariance), np.transpose(batch_X))
+      return np.diag(np.sqrt(square_norm))
+
+    def __update_batch(self, batch_X):
+        if self.fit_intercept:
+          return self.__add_intercept(batch_X)
+        return batch_X
+ 
+    def update_batch(self, batch_X):
+        if self.fit_intercept:
+          return self.__add_intercept(batch_X)
+        return batch_X
+
+
+    def get_loss(self, batch_X, batch_y):
+      self.__initialize_theta(batch_X)
+      batch_X = self.__update_batch(batch_X)    
+
+
+      z = np.dot(batch_X, self.theta)
+      h = self.__sigmoid(z)
+      return self.__loss(h, batch_y)
+  
+    def get_sigmoid_gradients(self, batch_X ):
+      self.__initialize_theta( batch_X)
+      batch_X = self.__update_batch(batch_X)    
+
+      z = np.dot(batch_X, self.theta)
+      h = self.__sigmoid(z)
+      gradients = batch_X.T * np.outer(  np.ones( batch_X.shape[1] ),  h)
+      return gradients
+
+
+    def get_gradient(self, batch_X, batch_y , inverse_probabilities = [None] ):
+      if inverse_probabilities[0] == None:
+        inverse_probabilities = np.ones(batch_X.shape[0])
+      self.__initialize_theta( batch_X)
+      batch_X = self.__update_batch(batch_X)    
+
+      z = np.dot(batch_X, self.theta)
+      h = self.__sigmoid(z)
+      gradient = np.dot(batch_X.T, inverse_probabilities*(h-batch_y))/batch_y.size
+      return gradient
+
+    def update(self,gradient, learning_rate):
+      self.theta -= learning_rate*gradient
+
+    def predict_prob(self, batch_X, inverse_data_covariance = []):
+      self.__initialize_theta(batch_X)
+      batch_X = self.__update_batch(batch_X) 
+      if len(inverse_data_covariance) == 0:     
+        return self.__sigmoid(np.dot(batch_X, self.theta))
+      else:
+        ### adjust this using the inverse_data_covariance
+        ### for each datapoint return the max 
+        return self.__sigmoid(np.dot(batch_X, self.theta) + self.alpha*self.__inverse_covariance_norm(batch_X, inverse_data_covariance))
 
 
     def get_predictions(self, batch_X, threshold, inverse_data_covariance = []):
